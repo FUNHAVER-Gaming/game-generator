f4133b9758e86d0f4446b4e486ba151e40baf7a9
TadahTech <tandis96@hotmail.com>
2022-05-30 01:54:33 -0500

    Add deletion to error messages

--- pkg/service/send_error_message.go
+++ pkg/service/send_error_message.go
@@ -2,13 +2,16 @@ package service
 
 import (
 	"fmt"
+	"time"
 )
 
 func sendError(msg, channel string) {
-	_, err := botSession.ChannelMessageSend(channel, "Error: "+msg)
+	resp, err := botSession.ChannelMessageSend(channel, "Error: "+msg)
 	if err != nil {
 		fmt.Printf("error sending error message %v", err.Error())
+		return
 	}
+	deleteMessage(channel, resp.ID, 10*time.Second)
 }
 
 func sendMessage(msg, channel string) string {
@@ -19,4 +22,15 @@ func sendMessage(msg, channel string) string {
 	}
 
 	return resp.ID
+}
+
+func deleteMessage(channel, id string, ttl time.Duration) {
+	go func() {
+		time.Sleep(ttl)
+		logWithArgs("Deleting message ID %v in channel %v", id, channel)
+		err := botSession.ChannelMessageDelete(channel, id)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
 }
