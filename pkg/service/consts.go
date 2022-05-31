c2e58de2bd38ce17fdc0a9b0c84895a5561238e8
TadahTech <tandis96@hotmail.com>
2022-05-31 00:14:12 -0500

    remove from master

--- pkg/service/consts.go
+++ pkg/service/consts.go
@@ -1,11 +1,5 @@
 package service
 
-import (
-	"fmt"
-	"github.com/joho/godotenv"
-	"os"
-)
-
 const (
 	JoviPcUserId = "910342510600151050"
 )
@@ -15,30 +9,30 @@ var (
 )
 
 func init() {
-	file, err := os.Open("../../dev.env")
-	if err != nil {
-		file, err = os.Open("../../prod.env")
-		if err != nil {
-			fmt.Println("Failed to find any .env file, failing")
-			os.Exit(-1)
-			return
-		}
-		fmt.Println("Did not find dev.env, using production server values")
-		isProduction = true
-	}
-
-	if file == nil {
-		fmt.Println("No .env file found, failing")
-		os.Exit(-1)
-		return
-	}
-
-	err = godotenv.Load(file.Name())
-
-	if file == nil {
-		fmt.Println(fmt.Sprintf("Failed to load env file %v", err.Error()))
-		os.Exit(-1)
-		return
-	}
+	//file, err := os.Open("../../dev.env")
+	//if err != nil {
+	//	file, err = os.Open("../../prod.env")
+	//	if err != nil {
+	//		fmt.Println("Failed to find any .env file, failing")
+	//		os.Exit(-1)
+	//		return
+	//	}
+	//	fmt.Println("Did not find dev.env, using production server values")
+	//	isProduction = true
+	//}
+	//
+	//if file == nil {
+	//	fmt.Println("No .env file found, failing")
+	//	os.Exit(-1)
+	//	return
+	//}
+	//
+	//err = godotenv.Load(file.Name())
+	//
+	//if file == nil {
+	//	fmt.Println(fmt.Sprintf("Failed to load env file %v", err.Error()))
+	//	os.Exit(-1)
+	//	return
+	//}
 
 }
